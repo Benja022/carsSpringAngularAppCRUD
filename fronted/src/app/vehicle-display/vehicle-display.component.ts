2155869d4c792a20dadcb292a897e91d833fba0c
Benja022 <113633123+Benja022@users.noreply.github.com>
2023-12-03 10:46:21 +0100

    My first Spring + Angular CRUD App

---
+++ fronted/src/app/vehicle-display/vehicle-display.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-display',
+  standalone: true,
+  imports: [],
+  templateUrl: './vehicle-display.component.html',
+  styleUrl: './vehicle-display.component.css'
+})
+export class VehicleDisplayComponent {
+
+}
